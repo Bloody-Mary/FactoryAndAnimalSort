d052a0251327443adadd14f9c0d350b5d64db3aa
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-20 21:56:48 +0300

    Updated class main in class _Main

--- src/main/java/ru/babushkina/factoryandanimalsort/_main/_Main.java
+++ src/main/java/ru/babushkina/factoryandanimalsort/_main/_Main.java
@@ -1,4 +1,30 @@
 package ru.babushkina.factoryandanimalsort._main;
 
+import ru.babushkina.factoryandanimalsort.model.Animal;
+import ru.babushkina.factoryandanimalsort.service.FactoryService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class _Main {
+    public static void main(String[] args) {
+        FactoryService factory = FactoryService.getInstance();
+        ArrayList<Animal> animals = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Animal animal = factory.createAnimal();
+            animals.add(animal);
+        }
+
+        System.out.println("Коллекция животных до сортировки:");
+        for (Animal animal : animals) {
+            animal.printInfo();
+        }
+
+        Collections.sort(animals);
+        System.out.println("Отсортированная коллекция животных по возрастанию:");
+        for (Animal animal : animals) {
+            animal.printInfo();
+        }
+    }
 }
