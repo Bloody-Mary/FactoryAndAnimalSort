dc01609d66ef2892e9d47747643e328a469f07e3
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-20 20:54:30 +0300

    Added method printInfo to class Dog

--- src/main/java/ru/babushkina/factoryandanimalsort/model/Dog.java
+++ src/main/java/ru/babushkina/factoryandanimalsort/model/Dog.java
@@ -1,5 +1,8 @@
 package ru.babushkina.factoryandanimalsort.model;
 
 public class Dog extends Animal {
-
+    @Override
+    public void printInfo() {
+        System.out.println("Собака: (" + getId() + ") возрастом " + getAge() + ", весом " + getWeight() + " и цветом " + getColor());
+    }
 }
