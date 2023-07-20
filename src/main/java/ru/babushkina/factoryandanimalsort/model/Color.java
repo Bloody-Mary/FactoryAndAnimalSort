34a9708adf7c8cb339f5af881640ad0982e99e0d
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-20 21:05:54 +0300

    Added variables and getter to class Color

--- src/main/java/ru/babushkina/factoryandanimalsort/model/Color.java
+++ src/main/java/ru/babushkina/factoryandanimalsort/model/Color.java
@@ -1,4 +1,17 @@
 package ru.babushkina.factoryandanimalsort.model;
 
-public class Color {
+public enum Color {
+    YELLOW(99),
+    WHITE(78),
+    BLACK(57),
+    BROWN(36);
+    private int popularity;
+
+    Color(int popularity) {
+        this.popularity = popularity;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
 }
