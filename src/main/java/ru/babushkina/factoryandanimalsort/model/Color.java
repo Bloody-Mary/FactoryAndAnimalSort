e1775cf391d3bc448a8d1c8a57964d83497c4288
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-21 16:15:44 +0300

    Updated enum Color

--- src/main/java/ru/babushkina/factoryandanimalsort/model/Color.java
+++ src/main/java/ru/babushkina/factoryandanimalsort/model/Color.java
@@ -1,11 +1,8 @@
 package ru.babushkina.factoryandanimalsort.model;
 
 public enum Color {
-    YELLOW(99),
-    WHITE(78),
-    BLACK(57),
-    BROWN(36);
-    private int popularity;
+    YELLOW(80), WHITE(40), BROWN(60), BLACK(90);
+    private final int popularity;
 
     Color(int popularity) {
         this.popularity = popularity;
