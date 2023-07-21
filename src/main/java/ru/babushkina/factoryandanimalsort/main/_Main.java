a8f9776f0df13b5b264c564aaa0f374204f8a2f9
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-21 16:13:28 +0300

    Created new class _Main

---
+++ src/main/java/ru/babushkina/factoryandanimalsort/main/_Main.java
@@ -0,0 +1,66 @@
+package ru.babushkina.factoryandanimalsort.main;
+
+import ru.babushkina.factoryandanimalsort.model.Animal;
+import ru.babushkina.factoryandanimalsort.model.AnimalType;
+import ru.babushkina.factoryandanimalsort.service.AnimalFactory;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class _Main {
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<>();
+        AnimalFactory animalFactory = AnimalFactory.getInstance();
+
+        for (AnimalType animalType : AnimalType.values()) {
+            Animal animal = animalFactory.createAnimal(animalType.name().toLowerCase());
+            animals.add(animal);
+        }
+
+        System.out.println("Заполненная коллекция животных:");
+
+        for (Animal animal : animals) {
+            System.out.println("Наше животное имеет следующие характеристики:");
+            System.out.println("ID: " + animal.getId());
+            System.out.println("Вид: " + animal.getClass().getSimpleName());
+            System.out.println("Возраст: " + animal.getAge() + " лет");
+            System.out.println("Вес: " + String.format("%.2f", animal.getWeight()) + " кг");
+//            System.out.println("Вес: " + animal.getWeight() + " кг");
+            System.out.println("Цвет: " + animal.getColor());
+            System.out.println("--------------------");
+        }
+
+        animals.sort(Comparator.comparingInt(Animal::getAge));
+
+        System.out.println("Отсортированная коллекция животных по возрастанию:");
+
+        for (Animal animal : animals) {
+            System.out.println("Наше животное:");
+            System.out.println("ID: " + animal.getId());
+            System.out.println("Тип: " + animal.getClass().getSimpleName());
+            System.out.println("Возраст: " + animal.getAge() + " лет");
+            System.out.println("Вес: " + String.format("%.2f", animal.getWeight()) + " кг");
+//            System.out.println("Вес: " + animal.getWeight() + " кг");
+            System.out.println("Цвет: " + animal.getColor());
+            System.out.println("--------------------");
+        }
+
+
+
+
+
+//        AnimalFactory animalFactory = AnimalFactory.getInstance();
+//
+//        ArrayList<Animal> animals = new ArrayList<>();
+//        for (AnimalType animalType : AnimalType.values()) {
+//            Animal animal = animalFactory.createAnimal(animalType.name().toLowerCase());
+//            animals.add(animal);
+//        }
+//
+//        for (Animal animal : animals) {
+//            System.out.println(animal);
+//            System.out.println("--------------------");
+//        }
+    }
+}
